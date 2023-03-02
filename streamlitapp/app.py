94f86915a13a969333d50cf20b39f5336bb68855
Nabil Osman <nabilosman14@gmail.com>
2023-03-02 20:53:25 +0000

    .

---
+++ streamlitapp/app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Football WebApp",
+    layout="centered",
+    initial_sidebar_state="expanded",
+)
+
+# title
+st.title('Welcome to My Football App!')
